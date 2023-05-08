732913be416107d35cbd86209c277e1c7c7cee55
omorikitty <omocatsun@gmail.com>
2023-05-07 21:24:54 -0500

    fix tags

--- bscfg/mods/admin.py
+++ bscfg/mods/admin.py
@@ -299,7 +299,7 @@ class Enhancement(bs.Actor):
             #         tag = u'\ue046TOP-RANK\ue046'
             #     PermissionEffect(owner=spaz.node, prefix=tag, prefixAnim={0: (
             #         1, 0, 0), 250: (0, 1, 0), 250*2: (0, 0, 1), 250*3: (1, 0, 0)})
-            if effectCustomers[cl_str]['tag'] == "" or cl_str not in effectCustomers:
+            if cl_str not in effectCustomers:
                 if cl_str in rol['owners']:
                     tag = getTag(1)
                     if tag == '0':
