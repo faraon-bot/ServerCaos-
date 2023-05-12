684b513e75362e3bb3b73ad56ee288d89cce5b9a
omorikitty <omocatsun@gmail.com>
2023-05-11 22:35:28 -0500

    Minor changes for the Triple Punch

--- bscfg/mods/bsRemoveTrubo.py
+++ bscfg/mods/bsRemoveTrubo.py
@@ -2,6 +2,7 @@ import bsSpaz
 from bsSpaz import *
 
 bsSpaz.Spaz.lastPunchTime = 0
+bsSpaz.gBasePunchCooldown = 400
 
 
 def onPunchPress(self):
@@ -11,6 +12,9 @@ def onPunchPress(self):
     """
     if not self.node.exists() or self.frozen or self.node.knockout > 0.0:
         return
+
+    if self.punchCallback is not None:
+        self.punchCallback(self)
     t = bs.getGameTime()
     self._punchedNodes = set()  # reset this..
     if t - self.lastPunchTime > self._punchCooldown:
@@ -18,9 +22,8 @@ def onPunchPress(self):
         self.node.punchPressed = True
         if not self.node.holdNode.exists():
             bs.gameTimer(
-                100,
-                bs.WeakCall(self._safePlaySound,
-                            self.getFactory().swishSound, 0.8))
+                100, bs.WeakCall(self._safePlaySound, self.getFactory().swishSound, 0.8)
+            )
 
 
 bsSpaz.Spaz.onPunchPress = onPunchPress
