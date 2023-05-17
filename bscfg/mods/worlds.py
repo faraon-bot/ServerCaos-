bee45d18ed88051ee3d7be419e42d8172ef126c5
omorikitty <omocatsun@gmail.com>
2023-05-17 18:39:35 -0500

    update worlds

--- bscfg/mods/worlds.py
+++ bscfg/mods/worlds.py
@@ -2,19 +2,13 @@
 import bs
 import settings
 import bsGame
-print 'Nigth Mode: ', settings.nightMode
-
-# ajust max players
-
-bsGame.Session.maxPlayers = 13
-bs.getConfig()['Free-for-All Max Players'] = 13
 
 
 def night():
     try:
         # print 'Nigth Mode: ', settings.nightMode
         if settings.nightMode:
-            bs.getSharedObject('globals').tint = (0.5, 0.7, 1.0)
+            bs.getSharedObject("globals").tint = (0.5, 0.7, 1.0)
     except:
         pass
 
